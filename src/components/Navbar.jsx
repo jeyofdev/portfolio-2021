b26ff7f65969a8481d6313809db189e8a53eb07d
jeyofdev <jgregoire.pro@gmail.com>
2021-01-16 11:25:43 +0100

    feat : add the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../styles/components/navbar.scss';
 
 const Navbar = () => {
-  return <div>navbar</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  const [classnameMenu, setClassnameMenu] = useState('menu menu--close');
+  const [classnameRevealer, setClassnameRevealer] = useState('block-revealer');
+  const [classnameMenuInner, setClassnameMenuInner] = useState('menu__inner');
+
+  const handleClickBtn = () => {
+    setIsOpen(!isOpen);
+    setClassnameMenu(isOpen ? 'menu menu--close' : 'menu menu--open');
+
+    setClassnameRevealer(
+      isOpen ? 'block-revealer animate-close' : 'block-revealer animate-open'
+    );
+
+    setClassnameMenuInner(
+      isOpen ? 'menu__inner animate-close' : 'menu__inner animate-open'
+    );
+
+    setTimeout(() => {
+      setClassnameRevealer('block-revealer');
+    }, 1000);
+  };
+
+  return (
+    <div className="Navbar">
+      <nav className={classnameMenu}>
+        {!isOpen ? (
+          <button
+            type="button"
+            className="btn btn--menu"
+            onClick={() => handleClickBtn()}
+          >
+            open
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="btn btn--close"
+            onClick={() => handleClickBtn()}
+          >
+            fermer
+          </button>
+        )}
+        <ul className={classnameMenuInner}>
+          <li className="menu__item">
+            <button type="button" className="menu__link">
+              Work
+            </button>
+          </li>
+          <li className="menu__item">
+            <button type="button" className="menu__link">
+              Play
+            </button>
+          </li>
+          <li className="menu__item">
+            <button type="button" className="menu__link">
+              Chat
+            </button>
+          </li>
+          <li className="menu__item">
+            <button type="button" className="menu__link">
+              Party
+            </button>
+          </li>
+        </ul>
+        <div className={classnameRevealer} />
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
