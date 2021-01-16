6479a8d33af275c6e525b338020784477d96b97d
jeyofdev <jgregoire.pro@gmail.com>
2021-01-16 12:39:07 +0100

    feat : set the navlink background

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import '../styles/components/navbar.scss';
+import navBg1 from '../img/1.jpg';
+import navBg2 from '../img/2.jpg';
+import navBg3 from '../img/3.jpg';
+import navBg4 from '../img/4.jpg';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [classnameMenu, setClassnameMenu] = useState('menu menu-close');
+  const [classnameMenu, setClassnameMenu] = useState('menu menu--close');
   const [classnameRevealer, setClassnameRevealer] = useState('block-revealer');
   const [classnameMenuInner, setClassnameMenuInner] = useState('menu-inner');
 
@@ -11,7 +15,7 @@ const Navbar = () => {
     e.target.classList.toggle('is-open');
 
     setIsOpen(!isOpen);
-    setClassnameMenu(isOpen ? 'menu menu-close' : 'menu menu-open');
+    setClassnameMenu(isOpen ? 'menu menu--close' : 'menu menu--open');
 
     setClassnameRevealer(
       isOpen ? 'block-revealer animate-close' : 'block-revealer animate-open'
@@ -24,6 +28,43 @@ const Navbar = () => {
     setTimeout(() => {
       setClassnameRevealer('block-revealer');
     }, 1000);
+  };
+
+  /**
+   * Change the background-image of the current link
+   *
+   * @param {SyntheticEvent} e
+   */
+  const handleHover = (e) => {
+    const ulElement = e.target.parentNode.parentNode.parentNode;
+
+    ulElement.classList.add('bg-active');
+
+    switch (e.target.classList[1]) {
+      case 'link-1':
+        ulElement.style.backgroundImage = `url(${navBg1})`;
+        break;
+      case 'link-2':
+        ulElement.style.backgroundImage = `url(${navBg2})`;
+        break;
+      case 'link-3':
+        ulElement.style.backgroundImage = `url(${navBg3})`;
+        break;
+      case 'link-4':
+        ulElement.style.backgroundImage = `url(${navBg4})`;
+        break;
+      default:
+        break;
+    }
+  };
+
+  /**
+   * Remove the background-image from current link
+   * @param {SyntheticEvent} e
+   */
+  const handleLeave = (e) => {
+    const ulElement = e.target.parentNode.parentNode.parentNode;
+    ulElement.style.backgroundImage = 'none';
   };
 
   return (
@@ -41,27 +82,53 @@ const Navbar = () => {
         </button>
         <ul className={classnameMenuInner}>
           <li className="menu-item">
-            <button type="button" className="menu-link">
-              <span className="menu-link-name">Home</span>
-              <span className="menu-link-label">Retour à l&apos;accueil</span>
+            <button
+              type="button"
+              className="menu-link link-1"
+              onMouseEnter={(e) => handleHover(e)}
+              onMouseLeave={(e) => handleLeave(e)}
+            >
+              <span className="menu-link-name link-1">Home</span>
+              <span className="menu-link-label link-1">
+                Retour à l&apos;accueil
+              </span>
             </button>
           </li>
           <li className="menu-item">
-            <button type="button" className="menu-link">
-              <span className="menu-link-name">Profile</span>
-              <span className="menu-link-label">Pour des informations</span>
+            <button
+              type="button"
+              className="menu-link link-2"
+              onMouseEnter={(e) => handleHover(e)}
+              onMouseLeave={(e) => handleLeave(e)}
+            >
+              <span className="menu-link-name link-2">Profile</span>
+              <span className="menu-link-label link-2">
+                Pour des informations
+              </span>
             </button>
           </li>
           <li className="menu-item">
-            <button type="button" className="menu-link">
-              <span className="menu-link-name">Compétences</span>
-              <span className="menu-link-label">Mes spécialitées</span>
+            <button
+              type="button"
+              className="menu-link link-3"
+              onMouseEnter={(e) => handleHover(e)}
+              onMouseLeave={(e) => handleLeave(e)}
+            >
+              <span className="menu-link-name link-3">Compétences</span>
+              <span className="menu-link-label link-3">Mes spécialitées</span>
             </button>
           </li>
           <li className="menu-item">
-            <button type="button" className="menu-link">
-              <span className="menu-link-name">Portfolio</span>
-              <span className="menu-link-label">Mes derniers travaux</span>
+            <button
+              type="button"
+              className="menu-link link-4"
+              onMouseEnter={(e) => handleHover(e)}
+              onMouseLeave={(e) => handleLeave(e)}
+            >
+              <span className="menu-link-name link-4">Portfolio</span>
+              <span className="menu-link-label link-4">
+                Mes derniers travaux
+              </span>
             </button>
           </li>
         </ul>
