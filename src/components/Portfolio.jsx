5aecb14657534445d4452147abcb6924c12cca30
jeyofdev <jgregoire.pro@gmail.com>
2021-01-23 18:57:15 +0100

    feat : create the Loader component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import OwlCarousel from 'react-owl-carousel';
 import works from '../datas/works';
 import Work from './reusable/Work';
+import Loader from './reusable/Loader';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 import '../styles/components/portfolio.scss';
@@ -9,6 +10,8 @@ import '../styles/components/portfolio.scss';
 const Portfolio = () => {
   return (
     <div className="portfolio">
+      <Loader />
+
       <OwlCarousel
         className="owl-theme"
         items={1}
---
+++ src/components/reusable/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../styles/components/reusable/loader.scss';
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <span />
+      <span />
+      <span />
+    </div>
+  );
+};
+
+export default Loader;
